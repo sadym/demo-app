079dd108d7de28333685f1d17a4e752293a62129
Patrick Lee Scott <pat@patscott.io>
2020-04-10 14:30:01 -0400

    feat(preview): load tests (#2)
    * feat(preview): load tests
    
    * chore: load test location
    
    * fix: load test output
    
    * fix: load test output
    
    * fix: preview url lowercased for strict ssl
    
    * fix(preview): load test github comment
    
    * fix(preview): load test github comment 2
    
    * fix(preview): load test github comment 3
    
    * fix(preview): rm env log
    
    * fix(preview): smaller test
    
    * fix(preview): eventloop hpa
    
    * fix(preview): eventloop higher cpu lower memory
    
    * fix(preview): limits
    
    * fix(preview): pods averagevalue test
    
    * fix(preview): pods averagevalue test
    
    * fix(preview): pod hpa syntax
    
    * fix: set container options for higher and guaranteed qos
    
    * fix: java/maven memory settings
    
    * fix: use go agent
    
    * fix: increase load test
    
    * fix: event loop delay target
    
    * fix: remove accidentally committed file
    
    * fix: set env vars from maven agent
    
    * fix: debug env
    
    * fix: DOCKER_REGISTRY env
    
    * fix: cleanup
    
    * fix: event loop delay target

---
+++ charts/preview/load-test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Downloading load testing tools..."
+
+curl -L https://github.com/starkandwayne/safe/releases/download/v1.3.4/safe-linux-amd64 --output ./safe-linux-amd64
+chmod +x ./safe-linux-amd64
+mv ./safe-linux-amd64 /usr/bin/safe
+
+wget https://bintray.com/loadimpact/rpm/rpm -O bintray-loadimpact-rpm.repo
+mv bintray-loadimpact-rpm.repo /etc/yum.repos.d/
+yum -y install k6
+
+echo "Configuring vault"
+jx get vault-config >> /dev/null
+eval `jx get vault-config`
+
+echo "Starting Load test with Preview URL: $PREVIEW_URL"
+
+url=$PREVIEW_URL \
+users=$LOAD_TEST_USERS \
+rampDuration=$LOAD_TEST_RAMP_DURATION \
+fullLoadDuration=$LOAD_TEST_FULL_LOAD_DURATION \
+k6 --quiet --summary-export load-test-results run load-test.js > load-test-output
+
+echo "Load test complete:"
+cat load-test-output
+
+slack_message=`cat load-test-output |  sed '/starting/,$!d'`
+
+curl --silent --data-urlencode "payload={
+\"text\": \"*Load Test Results:*
+
+A load test was run. To see the build logs that started this test run \`jx get build logs '$JOB_NAME #$BUILD_NUMBER'\`
+
+The load test was configured to simulate a maximum of $LOAD_TEST_USERS concurrent users. The number of users ramped up to the target number over $LOAD_TEST_RAMP_DURATION, ran at full capacity for $LOAD_TEST_FULL_LOAD_DURATION, and then ramped back down to 0 over the next $LOAD_TEST_RAMP_DURATION. 
+
+Here are the results of that test:
+
+\`\`\`
+$slack_message
+\`\`\`
+\"}
+" \
+--request POST \
+--url $(safe get secret/staging/k6:slackUrl)
+
+jx step pr comment -c "\`\`\``cat load-test-output`\`\`\`" -p $PULL_NUMBER -o $REPO_OWNER -r $REPO_NAME
